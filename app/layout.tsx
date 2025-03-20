da9fc164db088ed9b93a12e484221b7a03f659cb
Sha Rif Shaikh <77236625+ShaRifShykh@users.noreply.github.com>
2025-03-20 15:19:14 +0500

    Speed Insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import ThemeToggleButton from "./components/ThemeToggleButton";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const interFont = Inter({
   variable: "--font-inter",
@@ -27,6 +28,7 @@ export default function RootLayout({
           <ThemeToggleButton />
           {children}
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
